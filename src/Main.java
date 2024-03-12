907887a7ab92ce5fd4c76e724c7723ea6cf57f2b
Anastasia Chistyakova <kozlova27as@gmail.com>
2024-03-12 21:00:41 +0300

    formatting code

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,20 @@ public class Main {
         // на экран.
 
         int initAmount = 100;
-        //int deposit = 1100;
-        int deposit = 300;
+        int deposit = 1100;
+//        int deposit = 300;
         int bonus = 0;
-        int totalAmount =0;
-        if(deposit > 1000) {
+        int totalAmount = 0;
+        if (deposit > 1000) {
             int hundred = (int) Math.floor(deposit / 100);
-            bonus = hundred *1;
-            totalAmount = initAmount + initAmount + bonus;
+            bonus = hundred * 1;
+            totalAmount = initAmount + deposit + bonus;
             System.out.println("Итоговая сумма на счета абонента:" + totalAmount + "рублей");
-            System.out.println("Количество начисленных бонусов:"+ bonus);
-
-        }        else         {
+            System.out.println("Количество начисленных бонусов:" + bonus);
+        } else {
             totalAmount = initAmount + deposit;
             System.out.println("Итоговая сумма на счета абонента:" + totalAmount + "рублей");
-            System.out.println("Количество начисленных бонусов:"+ bonus);
+            System.out.println("Количество начисленных бонусов:" + bonus);
         }
     }
 }
